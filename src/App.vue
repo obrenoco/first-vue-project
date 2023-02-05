7cf8032400c361b1a9f1adbb28c0d12171c0935d
Breno Romeiro <bsromeiro@gmail.com>
2023-02-04 22:02:23 -0300

    add: basic style

--- src/App.vue
+++ src/App.vue
@@ -3,43 +3,79 @@ import { ref } from "vue";
 import type { Ref } from "vue";
 
 type ApiResponse = { status: string; message: string };
-const imageSrc: Ref<null | ApiResponse["message"]> = ref(null);
+
+const imageSrc: Ref<null | ApiResponse["message"]> = ref("");
+const isButtonDisabled = ref(false);
 
 async function fetchKitten() {
   imageSrc.value = null;
+  isButtonDisabled.value = true;
   try {
     const res = await fetch("https://dog.ceo/api/breeds/image/random");
     const imgLink: ApiResponse = await res.json();
     imageSrc.value = imgLink.message;
   } catch (error) {
     console.error("Error!");
+  } finally {
+    isButtonDisabled.value = false;
   }
 }
 </script>
 
 <template>
-  <div>
-    <img v-if="imageSrc" v-bind:src="imageSrc" alt="Beautiful cute dog" />
-    <p v-else>Loading...</p>
-    <button @click="fetchKitten">CLICK ME</button>
+  <div class="container">
+    <div v-if="imageSrc === null" class="loading">
+      <p>Loading...</p>
+    </div>
+    <div v-else-if="imageSrc === ''" class="empty"></div>
+    <img v-else v-bind:src="imageSrc" alt="Beautiful cute dog" />
+    <button @click="fetchKitten" :disabled="isButtonDisabled">CLICK ME</button>
   </div>
 </template>
 
 <style scoped>
-div {
+.container {
   display: flex;
   flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  gap: 20px;
+}
+
+.loading,
+img {
+  height: 300px;
+}
+
+button {
+  padding: 10px;
+  background-color: black;
+  font-weight: bold;
+}
+
+.loading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 img {
   width: 100%;
-  height: 500px;
+  object-fit: contain;
 }
 
 @media (max-width: 1024px) {
+
+  .loading,
+  img {
+    height: 150px;
+  }
+
   img {
     width: 100%;
-    height: 100%;
   }
 }
 </style>
